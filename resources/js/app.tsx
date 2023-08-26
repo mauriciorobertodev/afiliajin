fb14d8cef8f52e4c060ef64c607904817e41fee3
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-26 09:29:29 -0300

    remoção da barra de progresso

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -14,7 +14,5 @@ createInertiaApp({
         const root = createRoot(el);
         root.render(<App {...props} />);
     },
-    progress: {
-        color: "#4B5563",
-    },
+    progress: false,
 });
