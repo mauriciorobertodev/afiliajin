022a17701de7f8b943672b8af2de37f072fe60a3
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-26 09:30:55 -0300

    criação do layout principal e adição do mesmo nas páginas

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,20 +1,15 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { Header } from "@/components/header";
 
-export default function Dashboard({ auth }: PageProps) {
+export default function Dashboard({ auth, flash }: PageProps) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
+        <AuthenticatedLayout user={auth.user} notification={flash.notification}>
+            <Header heading="Dashboard" text="Gerencie suas páginas e popups." />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+            <div className="py-8">
+                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900">You're logged in!</div>
                 </div>
             </div>
         </AuthenticatedLayout>
--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -4,18 +4,17 @@ import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { Header } from "@/components/header";
 
 export default function Edit({
     auth,
     mustVerifyEmail,
     status,
+    flash,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
-        >
-            <Head title="Profile" />
+        <AuthenticatedLayout user={auth.user} notification={flash.notification}>
+            <Header heading="Perfil" text="Gerencie as configurações da conta." />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
