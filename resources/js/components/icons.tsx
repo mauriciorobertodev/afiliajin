053930b7299b1ac88ec26f4378c4e89729a8c1a4
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-26 09:27:10 -0300

    componente de icones

---
+++ resources/js/components/icons.tsx
@@ -0,0 +1,21 @@
+import { LayoutDashboard, LucideIcon, Settings, User, GalleryVerticalEnd, AppWindow, Plus, Check, ArrowLeft, ArrowRight, ChevronLeft, Loader2, X, CheckCircle, Info, AlertTriangle, XOctagon } from "lucide-react"
+
+export const Icons : { [key: string]: LucideIcon } = {
+    settings: Settings,
+    user: User,
+    dashboard: LayoutDashboard,
+    page: AppWindow,
+    popup: GalleryVerticalEnd,
+    add: Plus,
+    check: Check,
+    success: CheckCircle,
+    info: Info,
+    warning: AlertTriangle,
+    error: XOctagon,
+    arrowLeft: ArrowLeft,
+    arrowRight: ArrowRight,
+    chevronLeft: ChevronLeft,
+    spinner: Loader2,
+    x: X
+}
+
