2858a767997ecd367fab13f88c2beca786ea67cd
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-26 09:26:51 -0300

    header comum entre as páginas

---
+++ resources/js/components/header.tsx
@@ -0,0 +1,24 @@
+import { Head } from "@inertiajs/react"
+
+interface DashboardHeaderProps {
+  heading: string
+  text?: string
+  children?: React.ReactNode
+}
+
+export function Header({
+  heading,
+  text,
+  children,
+}: DashboardHeaderProps) {
+    return (
+        <div className="flex items-center justify-between mb-8">
+            <Head title={heading} />
+            <div className="grid gap-1">
+                <h1 className="font-heading text-3xl md:text-4xl font-bold">{heading}</h1>
+                {text && <p className="text-lg text-muted-foreground">{text}</p>}
+            </div>
+            {children}
+        </div>
+    )
+}
