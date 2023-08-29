f0475a173976d7e8309ce645e5bf133723894842
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-29 10:18:32 -0300

    adição do tipo de paginação e page para manipulação mais fácil

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -20,3 +20,29 @@ export type PageProps<T extends Record<string, unknown> = Record<string, unknown
         notification?: InertiaNotification;
     };
 };
+
+export interface WithPagination<T> {
+    current_page: number;
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: { url: string | null; label: string; active: boolean }[];
+    next_page_url: string | null;
+    path: string;
+    per_page: number;
+    prev_page_url: string | null;
+    to: number;
+    total: number;
+    data: T[];
+}
+
+export interface Page {
+    cloned_from: string;
+    created_at: string;
+    id: number;
+    name: string;
+    slug: string;
+    updated_at: string;
+    user_id: number;
+}
