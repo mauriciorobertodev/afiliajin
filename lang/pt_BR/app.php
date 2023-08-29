b3b1ca68b0286962f4aef6ceecf5c5a0fc3006ca
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-29 10:17:38 -0300

    adição de algumas mensagens de erro e sucesso

---
+++ lang/pt_BR/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'errors' => [
+        'unknown' => 'Erro desconhecido tente novamente',
+        'clone_fail' => 'Não foi possível clonar esta página, ou não existe ou produtor bloqueou essa possibilidade',
+        'clone_storage_fail' => 'Não foi possível salvar a página clonada no storage, check as configurações do storage',
+        'clone_database_fail' => 'Não foi possível salvar a página no banco de dados, check as configurações do banco',
+    ],
+    'success' => [
+        'cloned' => 'Página clonada com sucesso!'
+    ]
+];
