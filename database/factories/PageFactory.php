498ceeb185f2c1f29e8ba9b8ce4d1a8831926861
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-29 10:31:38 -0300

    factory para o model Page

---
+++ database/factories/PageFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Page>
+ */
+final class PageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'             => fake()->word(),
+            'slug'             => fake()->slug(),
+            'user_id'          => User::factory(),
+            'cloned_from'      => fake()->url(),
+            'created_at'       => fake()->dateTimeBetween('-1 weeks'),
+            'whatsapp_number'  => fake()->phoneNumber(),
+            'whatsapp_message' => fake()->text(),
+        ];
+    }
+}
