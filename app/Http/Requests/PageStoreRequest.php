2ea875568e59ceef442231b6dd14b362bb0482f6
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-30 09:22:31 -0300

    formatação

--- app/Http/Requests/PageStoreRequest.php
+++ app/Http/Requests/PageStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PageStoreRequest extends FormRequest
+final class PageStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,9 @@ class PageStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'min:3', 'max:255'],
-            'cloned_from' => ['required', 'active_url', 'max:255'],
-            'slug' => ['required', 'string', 'min:3', 'max:255']
+            'name'             => ['required', 'string', 'min:3', 'max:255'],
+            'cloned_from'      => ['required', 'active_url', 'max:255'],
+            'slug'             => ['required', 'string', 'min:3', 'max:255'],
         ];
     }
 }
