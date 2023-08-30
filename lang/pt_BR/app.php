881b75fc80d278ebc7cfdd56cc20a8dd772a422d
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-08-30 09:21:45 -0300

    adição de mensagem de validação

--- lang/pt_BR/app.php
+++ lang/pt_BR/app.php
@@ -2,12 +2,18 @@
 
 return [
     'errors' => [
-        'unknown' => 'Erro desconhecido tente novamente',
-        'clone_fail' => 'Não foi possível clonar esta página, ou não existe ou produtor bloqueou essa possibilidade',
-        'clone_storage_fail' => 'Não foi possível salvar a página clonada no storage, check as configurações do storage',
+        'unknown'             => 'Erro desconhecido tente novamente',
+        'clone_fail'          => 'Não foi possível clonar esta página, ou não existe ou produtor bloqueou essa possibilidade',
+        'clone_storage_fail'  => 'Não foi possível salvar a página clonada no storage, check as configurações do storage',
         'clone_database_fail' => 'Não foi possível salvar a página no banco de dados, check as configurações do banco',
     ],
     'success' => [
-        'cloned' => 'Página clonada com sucesso!'
-    ]
+        'page' => [
+            'cloned'    => 'Página clonada!',
+            'destroyed' => 'Página destruída',
+        ],
+    ],
+    'validations' => [
+        'whatsapp_number_required_if' => 'O número de whatsapp é necessário.',
+    ],
 ];
