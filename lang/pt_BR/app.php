bc37d673bcfcc2653874bd57f17c58a38f2b4858
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-07 13:54:47 -0300

    adição de traduções

--- lang/pt_BR/app.php
+++ lang/pt_BR/app.php
@@ -6,11 +6,13 @@ return [
         'clone_fail'          => 'Não foi possível clonar esta página, ou não existe ou produtor bloqueou essa possibilidade',
         'clone_storage_fail'  => 'Não foi possível salvar a página clonada no storage, check as configurações do storage',
         'clone_database_fail' => 'Não foi possível salvar a página no banco de dados, check as configurações do banco',
+        'deleted_content'     => 'Parece que o conteúdo da página foi excluído',
     ],
     'success' => [
         'page' => [
             'cloned'    => 'Página clonada!',
             'destroyed' => 'Página destruída',
+            'updated'   => 'Página atualizada',
         ],
     ],
     'validations' => [
