17a641c8823195d7747f76aa934c1d7b0c9ebaa6
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-11 10:45:03 -0300

    usando as ações de formação

--- app/Actions/Clone/FormatPageContent.php
+++ app/Actions/Clone/FormatPageContent.php
@@ -2,33 +2,32 @@
 
 namespace App\Actions\Clone;
 
+use App\Actions\Format\AlignDocument;
+use App\Actions\Format\FixLinks;
+use App\Actions\Format\PutBodyBottomTag;
+use App\Actions\Format\PutBodyTopTag;
+use App\Actions\Format\PutCookieTag;
+use App\Actions\Format\PutFontAwesome;
+use App\Actions\Format\PutHeadBottomTag;
+use App\Actions\Format\PutHeadTopTag;
+use App\Actions\Format\PutTailwindCssCDN;
+use App\Actions\Format\PutWhatsappButtonTag;
+
 final class FormatPageContent
 {
     public static function run(string $cloned_from, string $content): string
     {
-        // concertando os links relativos a path
-        $pattern = "/(href|src)\s*=\s*[\"']\s*(?:\.\/|\/|\.\.\/)(\w*\s*[^\"']+)[\"']/i";
-        $content = preg_replace($pattern, '$1="//' . $cloned_from . '$2"', $content);
+        $content = FixLinks::run($cloned_from, $content);
+        $content = PutFontAwesome::run($content);
+        $content = PutTailwindCssCDN::run($content);
+        $content = PutCookieTag::run($content);
+        $content = PutWhatsappButtonTag::run($content);
+        $content = PutHeadTopTag::run($content);
+        $content = PutHeadBottomTag::run($content);
+        $content = PutBodyTopTag::run($content);
+        $content = PutBodyBottomTag::run($content);
+        $content = AlignDocument::run($content);
 
-        // detectando se usa font awesome e definindo uma fonte free sem erro
-        $pattern = "/(font([-_\s]+)awesome|fontawesome)/i";
-        if (preg_match_all($pattern, $content)) {
-            $font    = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />';
-            $content = preg_replace('/([<]\s*\/\s*head\s*[>])/', $font . '$1', $content);
-        }
-
-        // colocando as tags que ficará códigos do usuário
-        $content = preg_replace('/([<]\s*head\s*[>])/', '<head>' . config('app.head_top_tag_start') . config('app.head_top_tag_end'), $content);
-        $content = preg_replace('/([<]\s*\/\s*head\s*[>])/', config('app.head_bottom_tag_start') . config('app.head_bottom_tag_end') . '</head>', $content);
-        $content = preg_replace('/([<]\s*body.*[>])/', '$1' . config('app.body_top_tag_start') . config('app.body_top_tag_end'), $content);
-        $content = preg_replace('/([<]\s*\/\s*body\s*[>])/', config('app.body_bottom_tag_start') . config('app.body_bottom_tag_end') . '</body>', $content);
-
-        // diminuindo o máximo de espaços em branco
-        $content = preg_replace("/(\s+)/", ' ', $content);
-
-        // quebrando a linha entre as tags
-        $content = preg_replace('/(\>\s*\<|\>\<)/', ">\n<", $content);
-
-        return trim($content);
+        return $content;
     }
 }
