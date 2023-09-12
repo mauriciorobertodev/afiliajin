e6cebd5010572cf9972c25a7c377dd294587342b
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-12 12:36:35 -0300

    adição de tradução

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -5,12 +5,10 @@ import { Header } from "@/components/header";
 export default function Dashboard({ auth, flash }: PageProps) {
     return (
         <AuthenticatedLayout user={auth.user} notification={flash.notification}>
-            <Header heading="Dashboard" text="Gerencie suas páginas e popups." />
+            <Header heading="Dashboard" />
 
             <div className="py-8">
-                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 text-gray-900">You're logged in!</div>
-                </div>
+                <div className=" text-gray-900">Você está logado!</div>
             </div>
         </AuthenticatedLayout>
     );
